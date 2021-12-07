b15ede1e632e1bdba7f9edf1debb85aa18560691
Harit Patel <75656894+Clinckop@users.noreply.github.com>
2021-12-07 16:17:21 +0530

    Edited form button lil bit

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -34,8 +34,10 @@ class SignIn extends React.Component{
 
                     <FormInput name="password" type="password" handleChange={this.handleChange} label="Password" value={this.state.password} />
                    
-                    <CustomButton type="submit">Sign In</CustomButton>
-                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign In with google</CustomButton>
+                    <div className="buttons">
+                        <CustomButton type="submit">Sign In</CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign In with google</CustomButton>   
+                    </div>
                 </form>
             </div>
         )
